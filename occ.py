0ff1de5561c101b7b211fe5e19629a5c1b884531
Yue Wang <xfdywy@users.noreply.github.com>
2023-10-19 16:36:20 +0800

    Create occ.py

---
+++ occ.py
@@ -0,0 +1,25 @@
+import torch
+import argparse
+ 
+def mat(gpu_idx):
+    device = torch.device(gpu_idx)
+    a = torch.randn(size=(1024, 10240)).to(device)
+    cnt = 0
+    while True:
+        a @ a.T
+        cnt += 1
+        if cnt % 1000 == 0:
+            a = torch.randn(size=(1024, 10240)).to(device)
+ 
+ 
+ 
+ 
+ 
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Pass an integer parameter to the script.")  
+    parser.add_argument("--deviceid", type=int, help="Integer parameter to be passed.")  
+ 
+ 
+ 
+    args = parser.parse_args()  
+    mat(args.deviceid)
